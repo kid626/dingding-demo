aef895d58c92e6b8d11877266d52c1cd42cf127d
fanzh <fanz23@chinaunicom.cn>
2021-07-22 13:42:16 +0800

    [bugfix] 修复 /dd ,/zzd 无法正常跳转的问题

--- src/main/java/com/bruce/dingding/demo/config/WebConfig.java
+++ src/main/java/com/bruce/dingding/demo/config/WebConfig.java
@@ -17,6 +17,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/zzd/*").setViewName("zzd/index.html");
+        registry.addViewController("/zzd").setViewName("zzd/index.html");
         registry.addViewController("/dd/*").setViewName("dd/index.html");
+        registry.addViewController("/dd").setViewName("dd/index.html");
     }
 }
